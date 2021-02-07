3e09f0002702bc3be9d1ffade1b8604dcd3f04c9
H0v <tonoyan6666@gmail.com>
2021-02-07 23:57:42 +0400

    added react-router

--- src/App.js
+++ src/App.js
@@ -4,15 +4,31 @@ import Catalogue from "./components/Catalogue/Catalogue";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Main />
-      <Catalogue />
-      <Bestsellers />
-      <Footer />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Header />
+            <Main />
+            <Catalogue />
+            <Bestsellers />
+            <Footer />
+          </Route>
+          <Route path="/furniture">
+            <Header />
+          </Route>
+          <Route path="/lightning">
+            <Header />
+          </Route>
+          <Route path="/*">
+            <h1>404</h1>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,22 +3,33 @@ import "./header.css";
 import Logo from "../../static/svg/Logo.svg";
 import CartIcon from "../../static/svg/CartIcon.svg";
 import SearchIcon from "../../static/svg/SearchIcon.svg";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header>
       <div className="headerBox">
-        <a href="/">
+        <Link to="/">
           <img className="logo" alt="Logo" src={Logo}></img>
-        </a>
+        </Link>
         <div className="navBar">
-          <button className="navItem fctm">Furniture</button>
-          <button className="navItem fctm">Lightning</button>
-          <button className="navItem fctm">Decor</button>
-          <button className="navItem fctm">Tablewear</button>
-          <button className="navItem">
-            <img alt="CartIcon" src={CartIcon}></img>
-          </button>
+          <Link to="/furniture">
+            <button className="navItem fctm">Furniture</button>
+          </Link>
+          <Link to="/lightning">
+            <button className="navItem fctm">Lightning</button>
+          </Link>
+          <Link to="/decor">
+            <button className="navItem fctm">Decor</button>
+          </Link>
+          <Link to="/tablewear">
+            <button className="navItem fctm">Tablewear</button>
+          </Link>
+          <Link to="/cart">
+            <button className="navItem">
+              <img alt="CartIcon" src={CartIcon}></img>
+            </button>
+          </Link>
           <button className="navItem">
             <img alt="SearchIcon" src={SearchIcon}></img>
           </button>
